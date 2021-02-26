1c9579887eee6635e657b2945a762e4799169c0c
cri_dei91 <cristina.deidda@polimi.it>
2021-02-26 15:16:21 +0100

    creation history folder

--- SUMMARY PER PAPER.R
+++ SUMMARY PER PAPER.R
@@ -1,4 +1,7 @@
 
+load("C:/PROJECTS 2021/QQ/Results_update/Max_annual_by_POT/lag3/M_ALL_newdataset.RData")
+
+
 #### FINAL NUMBERS FOR PAPER
 
 #num station considered
@@ -13,40 +16,78 @@ nrow(POT_matrix)
 
 nrow(M_ALL)
 
+#Negative values
+
+length(which(M_ALL$KendalT.value<0))
+
+100*length(which(M_ALL$KendalT.value<0))/ nrow(M_ALL)
+
+M_ALL_dep<-M_ALL[which(M_ALL$KendalT.p.value<=0.0017),]
+
 #Pvalue bonf
 
 Pv_th
 
 #Dependent couple
 
-N_dep<-nrow(DEP)
-N_ind<-nrow(IND)
+N_dep<-nrow(M_ALL_DEP)
+N_ind<-nrow(M_ALL_IND)
 
 
-nrow(DEP)+nrow(IND)== nrow(M_ALL)
+nrow(M_ALL_DEP)+nrow(M_ALL_IND)== nrow(M_ALL)
 
-100*nrow(DEP)/ nrow(M_ALL)
+100*nrow(M_ALL_DEP)/ nrow(M_ALL)
 
-100*nrow(IND)/ nrow(M_ALL)
+100*nrow(M_ALL_IND)/ nrow(M_ALL)
 
-length(which(DEP$KendalT.value<0))
+length(which(M_ALL_DEP$KendalT.value<0))
 
 
-DEP
+DEP<-M_ALL_DEP
 
-100*length(which(DEP$N.SY.N.ALL>=0.6))/nrow(DEP)
-100*length(which(DEP$N.SY.N.ALL>=0.4))/nrow(DEP)
+100*length(which(M_ALL_DEP$N.SY.N.ALL>=0.6))/nrow(M_ALL_DEP)
+100*length(which(M_ALL_DEP$N.SY.N.ALL>=0.4))/nrow(M_ALL_DEP)
 
 DEP_POS<-DEP[which(DEP$KendalT.value>0),]
 
 nrow(DEP_POS)
 
-DEP_near<-DEP_POS[which(DEP_POS$Distance<=230),]
-DEP_far<-DEP_POS[which(DEP_POS$Distance>230),]
+Dist_sel<-210
 
-DEP_asy20<-DEP_POS[which(DEP_POS$Num_Asyncr_occ>=20),]
+DEP_near<-M_ALL_DEP_POS[which(M_ALL_DEP_POS$Distance<=210),]
+DEP_far<-M_ALL_DEP_POS[which(M_ALL_DEP_POS$Distance>210),]
 
-max(DEP_asy20$N.SY.N.ALL)
 
-length(which(DEP_asy20$))
+summary(DEP_near$N.SY.N.ALL)
+summary(DEP_far$N.SY.N.ALL)
+
+summary(DEP_far$KendalT.value)
+summary(DEP_near$KendalT.value)
+
+
+DEP_asy20<-M_ALL_DEP_POS[which(M_ALL_DEP_POS$Num_Asyncr_occ>=20),]
+
+DEP_asy_less20<-M_ALL_DEP_POS[which(M_ALL_DEP_POS$Num_Asyncr_occ<20),]
+ 
+max(DEP_asy20$N.SY.N.ALL[which(DEP_asy20$X.1==1 | DEP_asy20$X.1==2 )])
+
+length(which(DEP_asy20$X.1==1))
+
+length(which(DEP_asy20$X.1==2))
+
+nrow(DEP_asy20)
+
+
+
+100*length(which(DEP_asy20$X.1==2))/
+  nrow(DEP_asy20)
+
+
+
+100*length(which(DEP_asy20$X.1==1))/
+  nrow(DEP_asy20)
+
+
+
+
 
